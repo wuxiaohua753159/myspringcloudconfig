381b83a390ad9d43c660c99dd15ec163776104a8
wxh <704742085@qq.com>
2018-07-29 17:00:35 +0800

    更新类

--- src/main/java/com/wu/test/SpringBootTestApplication.java
+++ src/main/java/com/wu/test/SpringBootTestApplication.java
@@ -3,6 +3,12 @@ package com.wu.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @Description:(这里用一句话描述这个类的作用)  
+ * @author :wxh  
+ * @date 2018年7月29日 上午11:22:50
+ */
 @SpringBootApplication
 public class SpringBootTestApplication {
 

ab84250961e88c2e2d5d937448877f2bdc6e308b
wxh <704742085@qq.com>
2018-07-29 17:54:32 +0800

    新增测试

--- src/main/java/com/wu/test/SpringBootTestApplication.java
+++ src/main/java/com/wu/test/SpringBootTestApplication.java
@@ -2,6 +2,8 @@ package com.wu.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
@@ -10,9 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2018年7月29日 上午11:22:50
  */
 @SpringBootApplication
+@RestController
 public class SpringBootTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
 	}
+	
+	
+	@RequestMapping("/hi")
+    public String home(String name) {
+        return "hi " + name + " ,i am from port:";
+    }
 }
